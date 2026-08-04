2de5cca9fd62ca2bbeca5ffe7ad01cefa0cf2ec6
ndrooo <git@ndr.ooo>
2026-08-04 11:32:00 -0400

    Add ellipse

--- src/p5ml.ts
+++ src/p5ml.ts
@@ -1,5 +1,6 @@
 import "/src/elements/canvas.ts";
 import "/src/elements/rect.ts";
 import "/src/elements/circle.ts";
+import "/src/elements/ellipse.ts";
 import "/src/elements/translate.ts";
 import "/src/elements/rotate.ts";
